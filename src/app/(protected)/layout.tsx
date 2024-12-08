4a693bc9c44791e73083c87613a2ebe20e00d822
Anirban Saha <88977035+Anirban-Saha@users.noreply.github.com>
2024-12-08 19:30:27 +0530

    .

--- src/app/(protected)/layout.tsx
+++ src/app/(protected)/layout.tsx
@@ -1,26 +1,30 @@
-import { SidebarProvider } from "@/components/ui/sidebar"
-import { UserButton } from "@clerk/nextjs"
-import React from "react"
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { UserButton } from "@clerk/nextjs";
+import AppSidebar from "./dashboard/app-sidebar";
 
-type Props={
-    children: React.ReactNode
-}
+type Props = {
+    children: React.ReactNode;
+};
 
-const SidebarLayout =({children}:Props)=>{
+const SidebarLayout = ({ children }: Props) => {
     return (
         <SidebarProvider>
+            <AppSidebar />
             <main className="w-full m-2">
-                <div className="flex items-center gap-2 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
-                    <div className="ml-auto">
-                        <UserButton/>
+                <div className="flex items-center gap-2 h-16 border-sidebar-border bg-sidebar border shadow rounded-md p-2 px-4">
+                    <div className="ml-auto"></div>
+                    {/* Increased UserButton size */}
+                    <div className="scale-125">
+                        <UserButton />
                     </div>
                 </div>
                 <div className="h-4"></div>
-                <div className='border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)]  p-4'>
+                <div className="border-sidebar-border bg-sidebar border shadow rounded-md overflow-y-scroll h-[calc(100vh-6rem)] p-4">
                     {children}
                 </div>
             </main>
         </SidebarProvider>
-    )
-}
-export default SidebarLayout
+    );
+};
+
+export default SidebarLayout;
---
+++ src/server/api/routers/project.ts
@@ -0,0 +1,27 @@
+import { z } from "zod";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
+
+export const projectRouter = createTRPCRouter({
+  createProject: protectedProcedure
+    .input(
+      z.object({
+        name: z.string(),
+        githubUrl: z.string().url("Invalid GitHub URL"),
+        githubToken: z.string().optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const project = await ctx.db.project.create({
+        data: {
+          name: input.name,
+          githubUrl: input.githubUrl,
+          userToProjects: {
+            create: {
+              userId: ctx.user.userId!,
+            },
+          },
+        },
+      });
+      return project;
+    }),
+});
